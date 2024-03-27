18378858927bf3a5ac2ff5e48a88369907fa20ea
Chandan Sharma <chandansharma79928.fea@gmail.com>
2024-03-27 16:41:04 +0530

    add some changes

--- src/page/footer-page/ContactUs.jsx
+++ src/page/footer-page/ContactUs.jsx
@@ -1,68 +1,100 @@
-import React from 'react'
-import DownLoadApp from '../homepage/download-app/DownLoadApp'
-import { Helmet } from 'react-helmet'
+import React from "react";
+import DownLoadApp from "../homepage/download-app/DownLoadApp";
+import { Helmet } from "react-helmet";
 
 function ContactUs() {
   return (
     <>
-  <Helmet>
-                
-          
-  <meta name="description" content="Contact Caross.in for unparalleled auto detailing services. Whether you have questions, want to schedule an appointment, or discuss your specific needs, our friendly team is ready to assist you. Experience excellence in automotive care with Caross.in."></meta>
-            
-            </Helmet>
+      <Helmet>
+        <meta
+          name="description"
+          content="Contact Caross.in for unparalleled auto detailing services. Whether you have questions, want to schedule an appointment, or discuss your specific needs, our friendly team is ready to assist you. Experience excellence in automotive care with Caross.in."
+        ></meta>
+      </Helmet>
 
+      <div className="container-fluid nav-margin">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="my-3 py-3">
+                <h3 className="red-t text-center">Contact Us</h3>
+              </div>
+              <p>
+                Thank you for considering Caross Car Detailing and Denting
+                Painting Studio for your automotive needs. We value your
+                interest and are here to provide you with exceptional service
+                and support. Feel free to get in touch with us using the
+                following contact details:
+              </p>
 
+              <div>
+                <h4>Contact Information</h4>
 
+                <ul>
+                  <li style={{ listStyleType: "initial" }}>
+                    {" "}
+                    <a href="tel:8929227077">
+                      <i
+                        class="fa-solid fa-phone-volume"
+                        style={{ color: "#b8242a" }}
+                      ></i>{" "}
+                      <span className="ms-3" style={{ color: "black" }}>
+                        {" "}
+                        +91 8929227077
+                      </span>
+                    </a>
+                  </li>
+                  <li style={{ listStyleType: "initial" }}>
+                    <a href="mailto:info@caross.in">
+                      {" "}
+                      <i
+                        class="fa-solid fa-envelope"
+                        style={{ color: "#b8242a" }}
+                      ></i>{" "}
+                      <span style={{ color: "black" }} className="ms-3">
+                        {" "}
+                        info@caross.in
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
 
-     <div className="container-fluid nav-margin">
-       <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className='my-3 py-3'>
-            <h3 className='red-t text-center'>Contact Us</h3>
-          </div>
-          <p>
-          Thank you for considering Caross Car Detailing and Denting Painting Studio for your automotive needs. We value your interest and are here to provide you with exceptional service and support. Feel free to get in touch with us using the following contact details:
-          </p>
-
-          <div>
-          <h4>
-            Contact Information
-          </h4>
-          
-          <ul>
-            <li style={{listStyleType:'initial'}}> <a  href="tel:8929227077"><i class="fa-solid fa-phone-volume" style={{color:'#b8242a'}}></i> <span className='ms-3' style={{color:'black'}}> +91 8929227077</span></a></li>
-            <li style={{listStyleType:'initial'}}><a  href="mailto:info@caross.in"> <i class="fa-solid fa-envelope" style={{color:'#b8242a'}}></i> <span style={{color:'black'}} className='ms-3'>  info@caross.in</span></a></li>
-          </ul>
-             </div>
+              <div>
+                <h4 className="mt-3">Merchant Legal Entity Name:</h4>
+                <p>ECOTIONS VENTURE PRIVATE LIMITED</p>
+                <div className="row">
+                  <div className="col-12 col-md-7">
+                    <h4 className="mt-3">Registered Address:</h4>
+                    <p>
+                      D 52 Street No 8 Rajnagar 2 Palam Colony South West Delhi
+                      DELHI 110077
+                    </p>
+                  </div>
+                  <div className="col-12 col-md-4">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28003.583150875642!2d77.05214017431643!3d28.676245599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05feaccc006f%3A0x247ab72f151e9b29!2sCAROSS%20DETAILING%20STUDIO!5e0!3m2!1sen!2sin!4v1696397201663!5m2!1sen!2sin"
+                      height="140"
+                      style={{ border: 0, maxWidth: "400px" }}
+                      allowfullscreen=""
+                      loading="lazy"
+                      referrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                </div>
+                <h4 className="mt-3">Operational Address:</h4>
+                <p>SRS 227 School Road, Peeragarhi, West Delhi, 110087</p>
               
-            <div>
-                <h4 className='mt-3'>Merchant Legal Entity Name:</h4>
-                  <p >ECOTIONS VENTURE PRIVATE LIMITED</p> 
-                  <div className='row'>
-                    <div className="col-12 col-md-7">
-              <h4 className='mt-3'>Registered Address:</h4>
-              <p >D 52 Street No 8 Rajnagar 2 Palam Colony South West Delhi DELHI 110077</p>
               </div>
-              <div className="col-12 col-md-4">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28003.583150875642!2d77.05214017431643!3d28.676245599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05feaccc006f%3A0x247ab72f151e9b29!2sCAROSS%20DETAILING%20STUDIO!5e0!3m2!1sen!2sin!4v1696397201663!5m2!1sen!2sin" height="140" style={{border:0, maxWidth:'400px'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-
-              </div>
-              </div>
-              <h4 className='mt-3'>Operational Address:</h4>
-              <p >SRS 227 School Road, Peeragarhi, West Delhi, 110087</p>
-            </div>
-
-            {/* <div>
+              {/* <div>
               <h4>Business Ours</h4>
               <p>Monday: &nbsp; Saturday 9:00 pm To 8:00 am</p>
               <p>Sunday : &nbsp; Close</p>
             </div> */}
-        </div>
+            </div>
 
-        {/* <div>
+            {/* <div>
           <h4>Connect With Us</h4>
           <p>Stay up to date with the latest news and automotive tips by following us on social media</p>
            <ul style={{listStyleType:'initial'}}>
@@ -72,22 +104,26 @@ function ContactUs() {
            </ul>
         </div> */}
 
-        {/* <div>
+            {/* <div>
           <h4>How To Reach Us</h4>
           <p>Our studio is conveniently located in West Delhi, making it easily accessible from various parts of the city. If you have any questions, need assistance, or would like to schedule an appointment, please don't hesitate to contact us via phone or email. Our knowledgeable team is here to assist you and ensure your experience with Caross Car Detailing and Denting Painting Studio is nothing short of exceptional.</p>
         </div> */}
-        <div>
-          <p>Thank you for considering us for your car care needs. We look forward to serving you and making your vehicle shine like new again.</p>
+            <div>
+              <p>
+                Thank you for considering us for your car care needs. We look
+                forward to serving you and making your vehicle shine like new
+                again.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-     </div>
 
-     {/* <div>
+      {/* <div>
       <DownLoadApp/>
      </div> */}
-     </>
-  )
+    </>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
--- src/page/homepage/contact-us/ContactUs.jsx
+++ src/page/homepage/contact-us/ContactUs.jsx
@@ -1,91 +1,185 @@
-import React, { useContext, useState } from 'react'
-import Toast from '../../../Tost';
-import Axios from '../../../Axios';
-import { AuthContext } from '../../../AuthProvider';
-import Loader from '../../../Loader';
+import React, { useContext, useState } from "react";
+import Toast from "../../../Tost";
+import Axios from "../../../Axios";
+import { AuthContext } from "../../../AuthProvider";
+import Loader from "../../../Loader";
 
-function ContactUs({childRef}) {
-  const [isLoading, setIsLoading] = useState(false)
- const [contactData, setContactData] = useState({
-  name:"",
-  mobile:"",
-  message:""
- })
-  const {userToken} = useContext(AuthContext)
-  const handleContactUs = async (e) =>{
-      e.preventDefault()
-    setIsLoading(true)
+function ContactUs({ childRef }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [contactData, setContactData] = useState({
+    name: "",
+    mobile: "",
+    message: "",
+  });
+  const { userToken } = useContext(AuthContext);
+  const handleContactUs = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
     try {
-        const response = await Axios.post('/contact', contactData,{
-         headers: {
-             Authorization: `Bearer ${userToken}`
-           }
-        }); 
-        if(response.status===200){
-         const data = response?.data;           
-         Toast(data?.message,response.status)  
-           setContactData({...contactData, name:"", mobile:"", message:""})         
-        }               
-       } catch (err) {
-         const error = err?.response?.data
-         Toast(error.message)
-       }finally{
-         setIsLoading(false)
-       }
-  }
+      const response = await Axios.post("/contact", contactData, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+      if (response.status === 200) {
+        const data = response?.data;
+        Toast(data?.message, response.status);
+        setContactData({ ...contactData, name: "", mobile: "", message: "" });
+      }
+    } catch (err) {
+      const error = err?.response?.data;
+      Toast(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
-   
-      <div ref={childRef} className="container-fluid py-5" id='contactpage'>
-         <div className="container py-3 px-4 card-shadow rounded" style={{backgroundColor:'#EDF5F9'}}>
-         <div  className="row">
-         <div className="col-12 col-md-6 py-3">
-               <h3 className='text-center py-3 text-md-start heading'><b className='red-t'>Contact</b> Information</h3>  
+      <div ref={childRef} className="container-fluid py-5" id="contactpage">
+        <div
+          className="container py-3 px-4 card-shadow rounded"
+          style={{ backgroundColor: "#EDF5F9" }}
+        >
+          <div className="row">
+            <div className="col-12 col-md-6 py-3">
+              <h3 className="text-center py-3 text-md-start heading">
+                <b className="red-t">Contact</b> Information
+              </h3>
               <div className="row">
                 <div className="col-12 py-3">
-                <a  href="tel:8929227077"><i class="fa-solid fa-phone-volume" style={{color:'#b8242a'}}></i> <span className='ms-3' style={{color:'black'}}> +91 8929227077</span></a>
+                  <a href="tel:8929227077">
+                    <i
+                      class="fa-solid fa-phone-volume"
+                      style={{ color: "#b8242a" }}
+                    ></i>{" "}
+                    <span className="ms-3" style={{ color: "black" }}>
+                      {" "}
+                      +91 8929227077
+                    </span>
+                  </a>
                 </div>
                 <div className="col-12 py-3">
-                <a  href="mailto:info@caross.in"> <i class="fa-solid fa-envelope" style={{color:'#b8242a'}}></i> <span style={{color:'black'}} className='ms-3'>  info@caross.in</span></a>
+                  <a href="mailto:info@caross.in">
+                    {" "}
+                    <i
+                      class="fa-solid fa-envelope"
+                      style={{ color: "#b8242a" }}
+                    ></i>{" "}
+                    <span style={{ color: "black" }} className="ms-3">
+                      {" "}
+                      info@caross.in
+                    </span>
+                  </a>
                 </div>
                 <div className="col-12 py-3">
-                
-                <a href="https://www.google.com/maps/place/CAROSS+DETAILING+STUDIO/@28.676246,77.090249,12z/data=!4m6!3m5!1s0x390d05feaccc006f:0x247ab72f151e9b29!8m2!3d28.6762456!4d77.090249!16s%2Fg%2F11trgqbptx?hl=en&entry=ttu"><i class="fa-solid fa-location-dot" style={{color:'#DE3820'}}></i><span style={{color:'black'}} className='ms-3'>SRS 227 , School Road, Peeragarhi West Delhi DELHI 110063</span></a>
+                  <a href="https://www.google.com/maps/place/CAROSS+DETAILING+STUDIO+%7C+GURGAON/@28.3985184,77.0762107,17z/data=!3m1!4b1!4m6!3m5!1s0x390d238737158797:0x40098e0aa8daa640!8m2!3d28.3985184!4d77.0762107!16s%2Fg%2F11vlpx_q7_?entry=ttu">
+                    <i
+                      class="fa-solid fa-location-dot"
+                      style={{ color: "#DE3820" }}
+                    ></i>
+                    <span style={{ color: "black" }} className="ms-3">
+                      Shop No 8 & 9, Plot No 25/1/1/1, opposite Dharam Tower,
+                      Sector 65,{" "}
+                      <span className="mx-4">Gurugram, Haryana 122102</span>
+                    </span>
+                  </a>
                 </div>
                 <div className="col-12">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28003.583150875642!2d77.05214017431643!3d28.676245599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d05feaccc006f%3A0x247ab72f151e9b29!2sCAROSS%20DETAILING%20STUDIO!5e0!3m2!1sen!2sin!4v1696397201663!5m2!1sen!2sin" height="140" style={{border:0, maxWidth:'400px'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3509.6870613700844!2d77.07621069999999!3d28.398518399999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d238737158797%3A0x40098e0aa8daa640!2sCAROSS%20DETAILING%20STUDIO%20%7C%20GURGAON!5e0!3m2!1sen!2sin!4v1711537656215!5m2!1sen!2sin"
+                    width="300"
+                    height="200"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                  ></iframe>
                 </div>
               </div>
-        </div>
-        <div className="col-12 col-md-6">
-              <h3 className='py-4 heading'><b className='red-t'>Get In Touch</b> With Us</h3>
+            </div>
+            <div className="col-12 col-md-6">
+              <h3 className="py-4 heading">
+                <b className="red-t">Get In Touch</b> With Us
+              </h3>
               <div className="row">
                 <div className="col-12">
-                <form>
-  <div className="mb-3 d-flex flex-column flex-md-row">
-   
-    <input type="text" value={contactData.name} onChange={(e)=>setContactData({...contactData, name:e.target.value})} className="form-control me-md-3" placeholder='Enter Full Name'  required />
-    <input type="number" value={contactData.mobile} onChange={(e)=>setContactData({...contactData, mobile:e.target.value})} className="form-control mt-3 mt-md-0" placeholder='Enter Phone Number'  required/>
-   
-  </div>
-  <div className="mb-3">
-   <textarea value={contactData.message} onChange={(e)=>setContactData({...contactData, message:e.target.value})} name="" id="" cols="12" rows="4" className='form-control' placeholder='Enter Your Query'></textarea> 
-  </div>
+                  <form>
+                    <div className="mb-3 d-flex flex-column flex-md-row">
+                      <input
+                        type="text"
+                        value={contactData.name}
+                        onChange={(e) =>
+                          setContactData({
+                            ...contactData,
+                            name: e.target.value,
+                          })
+                        }
+                        className="form-control me-md-3"
+                        placeholder="Enter Full Name"
+                        required
+                      />
+                      <input
+                        type="number"
+                        value={contactData.mobile}
+                        onChange={(e) =>
+                          setContactData({
+                            ...contactData,
+                            mobile: e.target.value,
+                          })
+                        }
+                        className="form-control mt-3 mt-md-0"
+                        placeholder="Enter Phone Number"
+                        required
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <textarea
+                        value={contactData.message}
+                        onChange={(e) =>
+                          setContactData({
+                            ...contactData,
+                            message: e.target.value,
+                          })
+                        }
+                        name=""
+                        id=""
+                        cols="12"
+                        rows="4"
+                        className="form-control"
+                        placeholder="Enter Your Query"
+                      ></textarea>
+                    </div>
 
-  <button type="submit" disabled={!((contactData.name && contactData.mobile) || isLoading)} onClick={(e)=>handleContactUs(e)} className="red-btn red-btn-block">
-  {isLoading? (<><span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Loading...</>):"Submit"}
-  </button>
-</form>
+                    <button
+                      type="submit"
+                      disabled={
+                        !((contactData.name && contactData.mobile) || isLoading)
+                      }
+                      onClick={(e) => handleContactUs(e)}
+                      className="red-btn red-btn-block"
+                    >
+                      {isLoading ? (
+                        <>
+                          <span
+                            class="spinner-border spinner-border-sm"
+                            role="status"
+                            aria-hidden="true"
+                          ></span>
+                          Loading...
+                        </>
+                      ) : (
+                        "Submit"
+                      )}
+                    </button>
+                  </form>
                 </div>
               </div>
+            </div>
+          </div>
         </div>
-         </div>
-           
-         </div>
       </div>
-      </>
-  )
+    </>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
